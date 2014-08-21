f2290530c074e91ca984d451a8de6d64a7d63e7c
Koji Tanaka <kj.tanaka@gmail.com>
2014-08-21 14:06:19 -0400

    adding recipe modulefile

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,7 @@ default['gnu_parallel']['version'] = VERSION
 default['gnu_parallel']['download_dir'] = "/root/source"
 default['gnu_parallel']['download_url'] = "http://ftp.gnu.org/gnu/parallel/parallel-#{VERSION}.tar.bz2"
 default['gnu_parallel']['prefix'] = "/opt/parallel-#{VERSION}"
+
+# Environment Modules
+default['gnu_parallel']['default_version'] = VERSION
+default['gnu_parallel']['modulefile_dir'] = "/opt/modules-3.2.10/Modules/3.2.10/modulefiles/parallel"
