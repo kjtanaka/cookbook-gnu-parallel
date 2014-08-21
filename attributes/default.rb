f5c855af2d74f4f53be86ce27c2d4d01d304afab
Koji Tanaka <kj.tanaka@gmail.com>
2014-08-21 13:40:25 -0400

    adding template and attribute

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+VERSION="20140722"
+default['gnu_parallel']['version'] = VERSION
+default['gnu_parallel']['download_dir'] = "/root/source"
+default['gnu_parallel']['download_url'] = "http://ftp.gnu.org/gnu/parallel/parallel-#{VERSION}.tar.bz2"
+default['gnu_parallel']['prefix'] = "/opt/parallel-#{VERSION}"
